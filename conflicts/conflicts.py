726796ab9ec020e7c039266e180ec60e00414086
Vasily Makarov <einmalfel@gmail.com>
2014-03-10 14:07:34 +0400

    turn main code into conflicts(list_of_heads) function

--- conflicts/conflicts.py
+++ conflicts/conflicts.py
@@ -63,88 +63,86 @@ def git_diff(from_treeish, to_treeish):
 def init():
     global regex
     regex = re.compile('^@@ -\S+ +\S+ @@.*$')
-bases = {}
-groups = []
 
-dprint("launched as: " + ' '.join(sys.argv))
 
-for head1 in (sys.argv[1:]):
-    for head2 in (sys.argv[sys.argv.index(head1) + 1:]):
-        bases[mix_name(head1, head2)] = git_base([head1, head2])
+def conflict(heads_list):
+    global regex
+    bases = {}
+    groups = []
 
-for mix in list(bases.keys()):
-    added_to_some_group = 0
+    for head1 in heads_list:
+        for head2 in heads_list[heads_list.index(head1) + 1:]:
+            bases[mix_name(head1, head2)] = git_base([head1, head2])
+
+    for mix in list(bases.keys()):
+        added_to_some_group = 0
+        for group in groups:
+            if group[0] == bases[mix]:
+                added_to_some_group = 1
+                for head1 in group[1:]:
+                    for head2 in unmix_name(mix):
+                        if not head1 == head2:
+                            if not group[0] == bases[mix_name(head1, head2)]:
+                                added_to_some_group = 0
+            if added_to_some_group:
+                group += (set(unmix_name(mix)) - set(group))
+                break
+        if not added_to_some_group:
+            groups.append([bases[mix]] + unmix_name(mix))
+
+    groups.sort(key=lambda group: group[0])
+
+    dprint("groups:")
+    dprint(groups)
+
+    prev_group = [0]
     for group in groups:
-        if group[0] == bases[mix]:
-            added_to_some_group = 1
-            for head1 in group[1:]:
-                for head2 in unmix_name(mix):
-                    if not head1 == head2:
-                        if not group[0] == bases[mix_name(head1, head2)]:
-                            added_to_some_group = 0
-        if added_to_some_group:
-            group += (set(unmix_name(mix)) - set(group))
-            break
-    if not added_to_some_group:
-        groups.append([bases[mix]] + unmix_name(mix))
+        dprint('processing group. Base: ' + group[0] +
+              ' Branches: ' + ', '.join(group[1:]))
+        if not prev_group[0] == group[0]:
+            diffs = {}
+        prev_group = group
+        diffs_to_add = set(group[1:]) - set(diffs.keys())
 
-groups.sort(key=lambda group: group[0])
+        for head in diffs_to_add:
+            dprint("reading diff for branch " + head)
+            diffs[head] = {}
+            for line in git_diff(group[0], head).split('\n'):
+                if 'diff --git a/' in line:
+                    filename = line.split('/')[-1]
+                    diffs[head][filename] = []
+                    dprint("processing file " + filename)
+                if regex.match(line):
+                    vprint('processing hunk ' + line)
+                    minus, plus = [part[1:] for part in line.split(' ')[1:3]]
+                    lines_removed = int(minus.split(',')[-1])\
+                            if ',' in minus else 0 if minus == '0' else 1
+                    if lines_removed:
+                        hunk_scope = minus
+                        lines_changed = int(lines_removed)
+                    else:
+                        hunk_scope = plus
+                        lines_changed = 1
+                    first_line = int(hunk_scope.split(',')[0])
+                    last_line = first_line + lines_changed
+                    diffs[head][filename] += [(first_line, last_line)]
+                    vprint('hunk scope is: ' + str(first_line) + '-' +
+                           str(last_line))
 
-dprint("groups:")
-dprint(groups)
-
-prev_group = [0]
-for group in groups:
-    dprint('processing group. Base: ' + group[0] +
-          ' Branches: ' + ', '.join(group[1:]))
-    if not prev_group[0] == group[0]:
-        diffs = {}
-    prev_group = group
-    diffs_to_add = set(group[1:]) - set(diffs.keys())
-
-    for head in diffs_to_add:
-        dprint("reading diff for branch " + head)
-        diffs[head] = {}
-        for line in git_diff(group[0], head).split('\n'):
-            if 'diff --git a/' in line:
-                filename = line.split('/')[-1]
-                diffs[head][filename] = []
-                dprint("processing file " + filename)
-            if regex.match(line):
-                vprint('processing hunk ' + line)
-                minus, plus = [part[1:] for part in line.split(' ')[1:3]]
-                lines_removed = int(minus.split(',')[-1])\
-                        if ',' in minus else 0 if minus == '0' else 1
-                if lines_removed:
-                    hunk_scope = minus
-                    lines_changed = int(lines_removed)
-                else:
-                    hunk_scope = plus
-                    lines_changed = 1
-                first_line = int(hunk_scope.split(',')[0])
-                last_line = first_line + lines_changed
-                diffs[head][filename] += [(first_line, last_line)]
-                vprint('hunk scope is: ' + str(first_line) + '-' +
-                       str(last_line))
-
-    comp_with = group[1:]
-    for head in group[1:]:
-        comp_with.remove(head)
-        dprint("comparing " + head + " with " + ', '.join(comp_with))
-        for filename in diffs[head].keys():
-            for head_to_comp in comp_with:
-                if filename not in diffs[head_to_comp].keys():
-                    continue
-                vprint("comparing changes to " + filename + " branch1: " +
-                       head + " (" + str(diffs[head][filename]) +
-                       ") branch2: " + head_to_comp +
-                       " (" + str(diffs[head_to_comp][filename]) + ")")
-                for comp_first, comp_last in diffs[head_to_comp][filename]:
-                    for first, last in diffs[head][filename]:
-                        if not (comp_last < first or comp_first > last):
-                            print(head + " conflicts with " +
-                                  head_to_comp + " in file " + filename)
-                            exit(1)
-
-print("no conflicts detected")
-exit(0)
+        comp_with = group[1:]
+        for head in group[1:]:
+            comp_with.remove(head)
+            dprint("comparing " + head + " with " + ', '.join(comp_with))
+            for filename in diffs[head].keys():
+                for head_to_comp in comp_with:
+                    if filename not in diffs[head_to_comp].keys():
+                        continue
+                    vprint("comparing changes to " + filename + " branch1: " +
+                           head + " (" + str(diffs[head][filename]) +
+                           ") branch2: " + head_to_comp +
+                           " (" + str(diffs[head_to_comp][filename]) + ")")
+                    for comp_first, comp_last in diffs[head_to_comp][filename]:
+                        for first, last in diffs[head][filename]:
+                            if not (comp_last < first or comp_first > last):
+                                return (head, head_to_comp, filename)
+    return None
