cca787616e75e05d9443885e70f0eda4d98a1fc4
Vasily Makarov <einmalfel@gmail.com>
2014-03-10 20:53:35 +0400

    setup.py: sync long_description with README.txt

--- conflicts/setup.py
+++ conflicts/setup.py
@@ -12,17 +12,5 @@ setup(name='conflicts',
       author="Vasily Makarov",
       author_email="einmalfel@gmail.com",
       url="https://github.com/einmalfel/git-aflow",
-      long_description="""
-Conflicts is a solution to check whether git branches conflict with each other
-or not without doing actual merge.
-conflict(list_of_heads) will return a tuple
-(HEAD1, HEAD2, file_containing_conflicting_changes) describing first found
-conflict or None if there are no conflicts at all.
-The package also provides do_not_conflict script. It returns 0 if its arguments
-are heads which do not conflict with each other, and 1 otherwise.
-Both function and script accept arguments in tree-ish form, for instance:
-master, 123abcde(SHA), HEAD^^.
-
-Written and tested with Python 3.3 runtime.
-"""
+      long_description=open("README.txt", "r").read(),
       )
