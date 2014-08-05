14c4327a09a50712833446c345219d6afee69c8c
Vasily Makarov <einmalfel@gmail.com>
2014-08-05 20:40:24 +0400

    gitwrapper: rework aux.py to make GitUnexpectedError more informative

--- gitwrapper/aux.py
+++ gitwrapper/aux.py
@@ -9,84 +9,49 @@ class GitUnexpectedError(Exception):
 
 def get_output_01(command_and_args):
     """Returns command output if it runs successfully, None if it returns 1"""
-    try:
-        result = subprocess.check_output(command_and_args,
-                                         stderr=subprocess.STDOUT).decode()[:-1]
-    except subprocess.CalledProcessError as error:
-        if error.returncode == 1:
-            result = None
-        else:
-            raise GitUnexpectedError(' '.join(error.cmd) + ' returns ' +
-                                     str(error.returncode) + '. Output: ' +
-                                     error.output) from error
-    except FileNotFoundError:
-        logging.critical('Command ' + command_and_args[0] + ' not found!')
-        raise
-    logging.debug('Calling ' + ' '.join(command_and_args)
-                  + '. Result: ' + str(result[1]) + linesep + 'Output:' +
-                  linesep + result[0])
-    return result
+    output, exit_code = get_output_and_exit_code(command_and_args)
+    if exit_code == 0:
+        return output
+    elif exit_code == 1:
+        return None
+    else:
+        raise GitUnexpectedError(' '.join(command_and_args) + ' returns ' +
+                                 str(exit_code) +
+                                 '. 0 or 1 expected. Output: ' + output)
 
 
 def check_01(command_and_args):
-    try:
-        subprocess.check_call(command_and_args, stdout=subprocess.DEVNULL,
-                              stderr=subprocess.DEVNULL)
-    except subprocess.CalledProcessError as error:
-        if error.returncode == 1:
-            return False
-        else:
-            raise GitUnexpectedError(' '.join(error.cmd) + ' returns ' +
-                                     str(error.returncode)) from error
-    except FileNotFoundError:
-        logging.critical('Command ' + command_and_args[0] + ' not found!')
-        raise
-    return True
+    output, exit_code = get_output_and_exit_code(command_and_args)
+    if exit_code == 0:
+        return True
+    elif exit_code == 1:
+        return False
+    else:
+        raise GitUnexpectedError(' '.join(command_and_args) + ' returns ' +
+                                 str(exit_code) +
+                                 '. 0 or 1 expected. Output: ' + output)
 
 
 def call(command_and_args):
-    try:
-        subprocess.check_call(command_and_args, stdout=subprocess.DEVNULL,
-                              stderr=subprocess.DEVNULL)
-    except subprocess.CalledProcessError as error:
-        raise GitUnexpectedError(' '.join(error.cmd) + ' returns ' +
-                                 str(error.returncode)) from error
-    except FileNotFoundError:
-        logging.critical('Command ' + command_and_args[0] + ' not found!')
-        raise
+    output, exit_code = get_output_and_exit_code(command_and_args)
+    if exit_code != 0:
+        raise GitUnexpectedError(' '.join(command_and_args) + ' returns ' +
+                                 str(exit_code) + '. Zero expected. Output: ' +
+                                 output)
 
 
 def get_output(command_and_args):
-    try:
-        result = subprocess.check_output(command_and_args,
-                                         stderr=subprocess.STDOUT).decode()[:-1]
-    except subprocess.CalledProcessError as error:
-        logging.critical('Command ' + ' '.join(command_and_args) +
-                         ' failed. Exit code: ' + str(error.returncode) +
-                         '. Output: ' + error.output.decode()[:-1])
-        raise
-    except FileNotFoundError:
-        logging.critical('Command ' + command_and_args[0] + ' not found!')
-        raise
-    logging.debug('Calling ' + ' '.join(command_and_args)
-                  + '. Result:' + linesep + result)
-    return result
+    output, exit_code = get_output_and_exit_code(command_and_args)
+    if exit_code != 0:
+        raise GitUnexpectedError(' '.join(command_and_args) + ' returns ' +
+                                 str(exit_code) + '. Zero expected. Output: ' +
+                                 output)
+    else:
+        return output
 
 
 def get_exit_code(command_and_args):
-    try:
-        subprocess.check_call(command_and_args, stdout=subprocess.DEVNULL,
-                              stderr=subprocess.DEVNULL)
-    except subprocess.CalledProcessError as error:
-        result = error.returncode
-    except FileNotFoundError:
-        logging.critical('Command ' + command_and_args[0] + ' not found!')
-        raise
-    else:
-        result = 0
-    logging.debug('Calling ' + ' '.join(command_and_args)
-                  + '. Result: ' + str(result))
-    return result
+    return get_output_and_exit_code(command_and_args)[0]
 
 
 def get_output_and_exit_code(command_and_args):
