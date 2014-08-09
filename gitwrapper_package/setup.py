19097a728df9d6f7e79decce149aef8a0aef1f2a
Vasily Makarov <einmalfel@gmail.com>
2014-08-09 14:36:58 +0400

    gitwrapper: add setup files

---
+++ gitwrapper_package/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+from distutils.core import setup
+
+
+setup(name='gitwrapper',
+      version='0.1',
+      description='Wrapper for git command line tool. For now functionality is '
+                  'limited to git commands needed for git-aflow project.',
+      packages=['gitwrapper'],
+      author="Vasily Makarov",
+      author_email="einmalfel@gmail.com",
+      url="https://github.com/einmalfel/git-aflow",
+      long_description=open("README.txt", "r").read(),
+      provides=["gitwrapper"],
+      )
