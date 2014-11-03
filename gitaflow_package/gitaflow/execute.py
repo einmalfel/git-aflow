49559d4973b80a724a3e81e79e792862da113c5c
Vasily Makarov <einmalfel@gmail.com>
2014-11-03 16:04:24 +0300

    implement git af topic continue

--- gitaflow_package/gitaflow/execute.py
+++ gitaflow_package/gitaflow/execute.py
@@ -5,7 +5,7 @@ import sys
 import traceback
 
 from gitwrapper import misc
-from gitaflow import init, merge, start
+from gitaflow import init, merge, start, continue_
 from gitaflow.common import die
 
 
@@ -58,6 +58,8 @@ def execute(args_namespace):
     elif args_namespace.subcommand == 'topic':
         if args_namespace.subsubcommand == 'start':
             start.start(args_namespace.name)
+        elif args_namespace.subsubcommand == 'continue':
+            continue_.continue_(args_namespace.name)
     elif args_namespace.subcommand == 'merge':
         merge.merge(args_namespace.source,
                     args_namespace.merge_type,
