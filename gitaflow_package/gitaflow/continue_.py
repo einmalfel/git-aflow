1b0c17b467bf96e452978fe430d08d8e73dc243d
Vasily Makarov <einmalfel@gmail.com>
2015-02-08 22:41:31 +0300

    continue: use last_m instead of last_r_cd as it has better semantics

--- gitaflow_package/gitaflow/continue_.py
+++ gitaflow_package/gitaflow/continue_.py
@@ -11,7 +11,7 @@ def continue_(name=None):
     head = commit.get_current_sha()
 
     logging.info('Looking for revision to continue..')
-    last_r_cd = None
+    last_m = None
     if name:
         nr = TopicRevision.from_branch_name(name)  # revision for name parsing
         ci = nr.iteration if nr.iteration else check_iteration()
@@ -19,9 +19,7 @@ def continue_(name=None):
             say('Version suffix ignored.')
         cd_all_merges = TopicMerge.get_all_merges_in(iteration.get_develop(ci))
         last_m = nr.topic.get_latest_merge(cd_all_merges, True)
-        if last_m:
-            last_r_cd = last_m.rev
-        else:
+        if not last_m:
             p_iters = tuple(i for i in iteration.get_iterations(sort=True)
                             if commit.is_ancestor(i, ci))
             # there is nothing before first iteration
@@ -32,37 +30,39 @@ def continue_(name=None):
                     if last_m:
                         logging.info('Found effective merge in master before ' +
                                      i + ': ' + str(last_m))
-                        last_r_cd = TopicRevision(nr.topic, ci,
-                                                  last_m.rev.version, ci)
                         break
             if not last_m:
                 die('Failed to find merges of ' + str(nr.topic) +
                     ' in iterations: ' + ', '.join((ci,) + p_iters) + '.')
-
     else:
         ci = check_iteration()
         for m in TopicMerge.get_all_merges_in(iteration.get_develop(ci)):
             if m.rev.SHA == head:
-                last_r_cd = m.rev
+                last_m = m
                 break
         else:
             die('No topic name was specified, neither HEAD is pointing to '
                 'last commit of some topic. Nothing to continue.')
 
-    logging.info('Last revision: ' + str(last_r_cd) + '. Checking branch '
+    logging.info('Last revision: ' + str(last_m.rev) + '. Checking branch '
                  'not exists and working tree is clean')
 
-    new_r = TopicRevision(last_r_cd.topic, None, last_r_cd.version + 1, ci)
+    # Let new_r.SHA contain SHA to start TB from. It's either RB or head of
+    # previous revision
+    new_r = TopicRevision(
+        last_m.rev.topic,
+        ci if commit.is_ancestor(last_m.rev.SHA, ci) else last_m.rev.SHA,
+        last_m.rev.version + 1, ci)
     tb_name = new_r.get_branch_name()
     if branch.exists(tb_name):
         die(tb_name + ' already exists. Use "git af checkout ' + tb_name +
             '" to continue your work on topic')
-    if not head == last_r_cd.SHA:
+    if not head == new_r.SHA:
         logging.info('Check working tree')
         check_working_tree_clean()
-        check_untracked_not_differ(last_r_cd.SHA)
-    last_v_ever = last_r_cd.topic.get_latest_merge(
-        last_r_cd.topic.get_all_merges()).rev.version
+        check_untracked_not_differ(new_r.SHA)
+    last_v_ever = new_r.topic.get_latest_merge(
+        new_r.topic.get_all_merges()).rev.version
     if last_v_ever >= new_r.version:
         say('Please, note that ' +
             new_r.topic.name + '_v' + str(new_r.version) +
@@ -74,7 +74,7 @@ def continue_(name=None):
 
     logging.info('All good, creating and checking out branch')
 
-    branch.create(tb_name, last_r_cd.SHA)
+    branch.create(tb_name, new_r.SHA)
     try:
         misc.checkout(tb_name)
     except:
